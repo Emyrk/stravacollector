8b673758e6134d666b0ff4a3f35848f790f5083a
Steven Masley <stevenmasley@coder.com>
2023-05-04 13:49:35 -0500

    Add chevron

--- site/strava-frontend/src/components/Navbar/AthleteAvatarDropdown.tsx
+++ site/strava-frontend/src/components/Navbar/AthleteAvatarDropdown.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react"
 import { AthleteSummary } from "../../api/typesGenerated"
-import { Text, Menu, MenuList, MenuItem, MenuButton, Button, useTheme, Container, Link } from "@chakra-ui/react"
+import { Text, Menu, MenuList, MenuItem, MenuButton, Flex, Button, useTheme, Container, Link } from "@chakra-ui/react"
 import { AthleteAvatar } from "../AthleteAvatar/AthleteAvatar"
-import { ChevronDownIcon } from "@chakra-ui/icons"
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRightFromBracket, faGear } from '@fortawesome/free-solid-svg-icons'
 import {
@@ -15,30 +15,44 @@ export const AthleteAvatarDropdown: FC<{
 }> = ({ athlete }) => {
 
   return <Menu>
-    {/* as={Button} rightIcon={<ChevronDownIcon />} */}
-    <MenuButton>
-      <AthleteAvatar
-        firstName={athlete.firstname}
-        lastName={athlete.lastname}
-        athleteID={athlete.athlete_id}
-        username={athlete.username}
-        profilePicLink={athlete.profile_pic_link}
-        hugelCount={athlete.hugel_count}
-        size="lg"
-      />
-    </MenuButton>
-    <MenuList>
-      <Link href="/logout" style={{ textDecoration: 'none' }}>
+    {({ isOpen }) => (<>
+      {/* as={Button} rightIcon={<ChevronDownIcon />} */}
+      <MenuButton
+        variant="none"
+        as={Button}
+        rightIcon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+      >
+        {/* <Flex alignItems={"center"}> */}
+        <AthleteAvatar
+          firstName={athlete.firstname}
+          lastName={athlete.lastname}
+          athleteID={athlete.athlete_id}
+          username={athlete.username}
+          profilePicLink={athlete.profile_pic_link}
+          hugelCount={athlete.hugel_count}
+          size="lg"
+        />
+        {/* {({ isOpen }) => (
+          <>
+            {isOpen && <ChevronDownIcon boxSize={9} />}
+          </>
+        )} */}
+        {/* </Flex> */}
+      </MenuButton>
+      <MenuList>
+        <Link href="/logout" style={{ textDecoration: 'none' }}>
+          <MenuItem>
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+            <Text as="span" paddingLeft={"10px"}>Logout</Text>
+          </MenuItem>
+        </Link>
         <MenuItem>
-          <FontAwesomeIcon icon={faArrowRightFromBracket} />
-          <Text as="span" paddingLeft={"10px"}>Logout</Text>
+          <FontAwesomeIcon icon={faGear} />
+          <Container paddingLeft={"10px"}>Settings</Container>
         </MenuItem>
-      </Link>
-      <MenuItem>
-        <FontAwesomeIcon icon={faGear} />
-        <Container paddingLeft={"10px"}>Settings</Container>
-      </MenuItem>
-    </MenuList>
+      </MenuList>
+    </>
+    )}
   </Menu >
 }
 
