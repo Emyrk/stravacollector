04bd2f502679881e5816ebf4f16c962488e7de48
Steven Masley <stevenmasley@gmail.com>
2024-10-24 21:45:10 -0400

    use types, not json

--- database/customModels.go
+++ database/customModels.go
@@ -2,10 +2,38 @@ package database
 
 import (
 	"database/sql/driver"
+	"encoding/json"
+	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/lib/pq"
 )
+
+type SegmentEfforts struct {
+	ActivityID   string    `json:"activity_id"`
+	EffortID     string    `json:"effort_id"`
+	StartDate    time.Time `json:"start_date"`
+	SegmentID    string    `json:"segment_id"`
+	ElapsedTime  int       `json:"elapsed_time"`
+	MovingTime   int       `json:"moving_time"`
+	DeviceWatts  bool      `json:"device_watts"`
+	AverageWatts float64   `json:"average_watts"`
+}
+
+func (a *SegmentEfforts) Scan(src interface{}) error {
+	switch v := src.(type) {
+	case string:
+		return json.Unmarshal([]byte(v), &a)
+	case []byte:
+		return json.Unmarshal(v, &a)
+	}
+	return fmt.Errorf("unexpected type %T", src)
+}
+
+func (a *SegmentEfforts) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
 
 type Floats []float64
 
--- database/models.go
+++ database/models.go
@@ -247,11 +247,11 @@ type GueJob struct {
 }
 
 type HugelActivity struct {
-	ActivityID       int64           `db:"activity_id" json:"activity_id"`
-	AthleteID        int64           `db:"athlete_id" json:"athlete_id"`
-	SegmentIds       interface{}     `db:"segment_ids" json:"segment_ids"`
-	TotalTimeSeconds int64           `db:"total_time_seconds" json:"total_time_seconds"`
-	Efforts          json.RawMessage `db:"efforts" json:"efforts"`
+	ActivityID       int64          `db:"activity_id" json:"activity_id"`
+	AthleteID        int64          `db:"athlete_id" json:"athlete_id"`
+	SegmentIds       interface{}    `db:"segment_ids" json:"segment_ids"`
+	TotalTimeSeconds int64          `db:"total_time_seconds" json:"total_time_seconds"`
+	Efforts          SegmentEfforts `db:"efforts" json:"efforts"`
 }
 
 type Map struct {
