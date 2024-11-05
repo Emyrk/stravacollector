deee4711e0ef0908bcd7e944c036c69169d4c09d
Steven Masley <stevenmasley@gmail.com>
2024-11-05 10:16:55 -0600

    fix sign out background color

--- site/strava-frontend/src/pages/SignedOut/SignedOut.tsx
+++ site/strava-frontend/src/pages/SignedOut/SignedOut.tsx
@@ -1,44 +1,58 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Heading, useTheme } from "@chakra-ui/react";
-import { FC } from "react"
 import {
-  Link as RouteLink
-} from "react-router-dom";
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Button,
+  Heading,
+  useTheme,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { Link as RouteLink } from "react-router-dom";
 
 export const SignedOut: FC = () => {
-  const theme = useTheme()
+  const theme = useTheme();
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        minHeight: '100vh',
-        backgroundColor: theme.colors.brand.primary,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        minHeight: "100vh",
+        backgroundColor: "#1a202c", //theme.colors.brand.primary,
       }}
     >
       <Alert
-        status='success'
-        variant='subtle'
-        flexDirection='column'
-        alignItems='center'
-        justifyContent='center'
-        textAlign='center'
-        height='200px'
+        status="success"
+        variant="subtle"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        height="200px"
         backgroundColor="transparent"
       >
-        <AlertIcon boxSize='40px' mr={0} />
-        <AlertTitle mt={4} mb={1} fontSize='lg'>
+        <AlertIcon boxSize="40px" mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize="lg">
           Signed Out
         </AlertTitle>
-        <AlertDescription maxWidth='sm'>
+        <AlertDescription maxWidth="sm">
           You have been sucessfully signed out.
         </AlertDescription>
         <RouteLink to="/">
-          <Button size={"lg"} textColor={theme.colors.brand.primary} marginTop={7} backgroundColor={theme.colors.brand.secondary}>Back Home</Button>
+          <Button
+            size={"lg"}
+            textColor={theme.colors.brand.primary}
+            marginTop={7}
+            backgroundColor={theme.colors.brand.secondary}
+          >
+            Back Home
+          </Button>
         </RouteLink>
       </Alert>
-    </Box >
+    </Box>
   );
-}
+};
