7bbc5d36f2e9b9d4e06242ac6cca819502706564
Steven Masley <stevenmasley@gmail.com>
2024-11-06 11:38:53 -0600

    update www redirect to preserve path

--- api/httpmw/www.go
+++ api/httpmw/www.go
@@ -2,6 +2,7 @@ package httpmw
 
 import (
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -10,7 +11,15 @@ func NoWWW() func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if strings.HasPrefix(r.Host, "www.") {
 				// Just send back to the non-www version.
-				http.Redirect(w, r, "https://"+strings.TrimPrefix(r.Host, "www."), http.StatusTemporaryRedirect)
+				rd, err := url.Parse("https://" + strings.TrimPrefix(r.Host, "www."))
+				if err != nil {
+					http.Redirect(w, r, "https://"+strings.TrimPrefix(r.Host, "www."), http.StatusTemporaryRedirect)
+					return
+				}
+				rd.Path = r.URL.Path
+				rd.RawQuery = r.URL.RawQuery
+
+				http.Redirect(w, r, rd.String(), http.StatusTemporaryRedirect)
 				return
 			}
 
