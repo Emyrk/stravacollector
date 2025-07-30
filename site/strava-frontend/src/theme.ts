c84a2bc0eee48c7d3788b2e60e33cc2d9e85e71f
Steven Masley <stevenmasley@gmail.com>
2025-07-30 15:16:53 -0500

    idk

---
+++ site/strava-frontend/src/theme.ts
@@ -0,0 +1,12 @@
+import { createSystem, defaultConfig } from "@chakra-ui/react"
+
+export const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        heading: { value: `'Figtree', sans-serif` },
+        body: { value: `'Figtree', sans-serif` },
+      },
+    },
+  },
+})
